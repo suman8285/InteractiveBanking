2541d89c4f7b8df96cec1dfe8779f85a3066607b
suman <suman8285@gmail.com>
2018-09-05 00:01:28 +0530

    empty project

---
+++ src/main/java/com/training/interactiveBanking/interactiveBanking/InteractiveBankingApplication.java
@@ -0,0 +1,12 @@
+package com.training.interactiveBanking.interactiveBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveBankingApplication.class, args);
+	}
+}
