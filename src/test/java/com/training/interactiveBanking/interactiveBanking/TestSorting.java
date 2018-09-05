0ae0819dddc23873285f37482b6c4ab6af281158
suman8285 <suman8285@gmail.com>
2018-09-05 10:07:35 +0530

    testing sort

---
+++ src/test/java/com/training/interactiveBanking/interactiveBanking/TestSorting.java
@@ -0,0 +1,56 @@
+package com.sc.test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestSorting {
+
+	public static void main(String[] args) {
+
+		List<Developer> listDevs = getDevelopers();
+		
+		System.out.println("Before Sort");
+		for (Developer developer : listDevs) {
+			System.out.println(developer.toString());
+		}
+		
+		System.out.println("After Sort");
+		
+		//lambda here!
+		//listDevs.sort((Developer o1, Developer o2)->o1.getAge()-o2.getAge());
+		
+		//lambda, valid, parameter type is optional
+		listDevs.sort((o1, o2)->o1.getAge()-o2.getAge());
+	
+		//java 8 only, lambda also, to print the List
+		listDevs.forEach(System.out::println);
+		
+		System.out.println("After Sort of salary");
+		//lambda
+		listDevs.sort((o1, o2)->o1.getSalary().compareTo(o2.getSalary()));
+		//java 8 only, lambda also, to print the List
+		listDevs.forEach((developer)->System.out.println(developer));
+		
+		System.out.println("After Sort of salary reverse");
+		Comparator<Developer> salaryComparator = (o1, o2)->o1.getSalary().compareTo(o2.getSalary());
+		listDevs.sort(salaryComparator.reversed());
+		listDevs.forEach((developer)->System.out.println(developer));
+	}
+
+	private static List<Developer> getDevelopers() {
+
+		List<Developer> result = new ArrayList<Developer>();
+
+		result.add(new Developer("mkyong", new BigDecimal("70000"), 33));
+		result.add(new Developer("alvin", new BigDecimal("80000"), 20));
+		result.add(new Developer("jason", new BigDecimal("100000"), 10));
+		result.add(new Developer("iris", new BigDecimal("170000"), 55));
+		result.add(new Developer(null,null,0));
+		
+		return result;
+
+	}
+	
+}
